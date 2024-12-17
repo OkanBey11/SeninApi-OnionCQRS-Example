a26fafc7331ad4bd6953fbb79825f8a641ef413f
OkanBey11 <okangoztok@hotmail.com>
2024-12-17 17:29:25 +0300

    Entities Created

---
+++ Core/SeninApi.Domain/Entities/Brand.cs
@@ -0,0 +1,20 @@
+﻿using SeninApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeninApi.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+
+        public Brand() { }
+        public Brand(string name) 
+        {
+            Name = name; 
+        }
+        public required string Name { get; set; }
+    }
+}
