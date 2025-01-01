0588f2c1d5d5f1fc3d6b7ebdbd2818a734ea037c
OkanBey11 <okangoztok@hotmail.com>
2025-01-01 16:10:14 +0300

    Redis Addimg

---
+++ Infrastructure/SeninApi.Infrastructure/RedisCache/RedisCacheService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using SeninApi.Application.Interfaces.RedisCache;
+using StackExchange.Redis;
+
+namespace SeninApi.Infrastructure.RedisCache
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly ConnectionMultiplexer redisConnection;
+        private readonly IDatabase database;
+        private readonly RedisCacheSettings settings;
+
+        public RedisCacheService(IOptions<RedisCacheSettings> options,ConnectionMultiplexer redisConnection, IDatabase database, RedisCacheSettings settings)
+        {
+            settings = options.Value;
+            var opt = ConfigurationOptions.Parse(settings.ConnectionString);
+            redisConnection = ConnectionMultiplexer.Connect(opt);
+            database = redisConnection.GetDatabase();
+        }
+        public async Task<T> GetAsync<T>(string key)
+        {
+            var value = await database.StringGetAsync(key);
+            if (value.HasValue)
+                return JsonConvert.DeserializeObject<T>(value);
+
+            return default;
+        }
+
+        public async Task SetAsync<T>(string key, T value, DateTime? exprationTime = null)
+        {
+            TimeSpan timeUnitExpiration = exprationTime.Value - DateTime.Now;
+            await database.StringSetAsync(key, JsonConvert.SerializeObject(value), timeUnitExpiration);
+        }
+    }
+}
